06c53e55cbda373cbbae3ae3c0737d32f14bdd64
usha1116 <ushasahu1116@gmail.com>
2025-01-07 23:21:52 +0530

    css update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom"
+import Navbar from "./components/Navbar"
 
 
 function App() {
 
   return (
     <>
-    <nav>Navbar</nav>
+    <Navbar/>
     <main className='min-h-screen max-w-screen-2x1 mx-auto px-4 py-6 font-primary ' >
       <Outlet/>
       </main>
